a659c0663088a7d4d1c5e90956ef4e56e1300c15
anton-mladenov <tonkata@mail.com>
2018-07-13 11:00:01 +0200

    env fixes

--- src/db.ts
+++ src/db.ts
@@ -28,8 +28,8 @@ class CustomNamingStrategy extends DefaultNamingStrategy implements NamingStrate
 export default () =>
   createConnection({
     type: "postgres",
-    url: process.env.DATABASE_URL,
-    // url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
+    // url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
     entities: [
       User,
       Player,
