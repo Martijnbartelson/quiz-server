8c01cd7bb5dcc7310429f6e3779e069da0d0c75a
anton-mladenov <tonkata@mail.com>
2018-07-13 10:14:15 +0200

    commented out port 4000 from db.ts

--- src/db.ts
+++ src/db.ts
@@ -28,7 +28,7 @@ class CustomNamingStrategy extends DefaultNamingStrategy implements NamingStrate
 export default () =>
   createConnection({
     type: "postgres",
-    url: process.env.DATABASE_URL || 'postgres://apddcuefuogsiw:2874ced033b88961f1b35aeef2a71bf9b0ffc7874932b9003d9939984b9a740d@ec2-54-217-235-137.eu-west-1.compute.amazonaws.com:5432/d1laiu0ibaal7m',
+    url: process.env.DATABASE_URL,
     // url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
     entities: [
       User,
